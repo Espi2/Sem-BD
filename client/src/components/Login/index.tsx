c330c78199abe49ca095b454922784ee06ac287d
AlejandroPadilla26 <148174177+AlejandroPadilla26@users.noreply.github.com>
2024-05-20 21:27:43 -0600

    Update index.tsx
    Cambié unos cuantos textos, simplemente para que quedará en español.

--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [usuario, setUsuario] = useState("");
   const [contrasena, setContrasena] = useState("");
   const [error, setError] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,8 +18,10 @@ function Login() {
         contrasena,
       });
       console.log(response.data);
-
-      setIsLoggedIn(true);
+      if (response.data.message === "Autenticación correcta") {
+        setIsLoggedIn(true);
+        localStorage.setItem("isLoggedIn", "true"); 
+      }
     } catch (error) {
       setError("Usuario o contraseña incorrectos");
 
@@ -27,50 +31,60 @@ function Login() {
     }
   };
 
-  if (isLoggedIn) {
-    //return redirect("/");
-    // console.log("redireccion");
-  }
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/menu");
+    }
+  }, [navigate, isLoggedIn]);
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Sign In</h1>
-      <form
-        className={styles.form}
-        onSubmit={handleSubmit}
-        style={{ marginTop: error ? "75px" : "0" }}
-      >
-        <div className={styles.inputGroup}>
-          <label htmlFor="inputUsername" className={styles.label}>
-            Usuario
-          </label>
-          <input
-            type="text"
-            className={styles.input}
-            id="inputUsername"
-            value={usuario}
-            onChange={(e) => setUsuario(e.target.value)}
-            placeholder="Usuario"
-          />
-        </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="inputPassword" className={styles.label}>
-            Contraseña
-          </label>
-          <input
-            type="password"
-            className={styles.input}
-            id="inputPassword"
-            value={contrasena}
-            onChange={(e) => setContrasena(e.target.value)}
-            placeholder="Contraseña"
-          />
-        </div>
-        <button type="submit" className={styles.btn}>
-          Login
-        </button>
-      </form>
-      {error && <div className="alert alert-danger">{error}</div>}
+    <div>
+      <div>
+        <nav className="navbar bg-body-tertiary">
+          <div className="container-fluid">
+            <span className="navbar-letra mb-0 h1">Coto Granate</span>
+          </div>
+        </nav>
+      </div>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Inicio de Sesión</h1>
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit}
+          style={{ marginTop: error ? "75px" : "0" }}
+        >
+          <div className={styles.inputGroup}>
+            <label htmlFor="inputUsername" className={styles.label}>
+              Nombre de Usuario
+            </label>
+            <input
+              type="text"
+              className={styles.input}
+              id="inputUsername"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+              placeholder="Nombre de Usuario"
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="inputPassword" className={styles.label}>
+              Contraseña
+            </label>
+            <input
+              type="password"
+              className={styles.input}
+              id="inputPassword"
+              value={contrasena}
+              onChange={(e) => setContrasena(e.target.value)}
+              placeholder="Contraseña"
+            />
+          </div>
+          <button type="submit" className={styles.btn}>
+            Iniciar
+          </button>
+        </form>
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
     </div>
   );
 }
